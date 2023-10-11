1089b8d53cbc108aa0f4e26ffbe8ef668e3eb2a1
sean-madu <madusean2@gmail.com>
2023-10-11 13:31:36 -0600

    Add bootstrap to project

--- django_project/react_app/src/App.js
+++ django_project/react_app/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import './components/ListGroup'
+import ListGroup from './components/ListGroup';
 function App() {
   return (
 
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListGroup />
     </div>
 
   );
---
+++ django_project/react_app/src/components/ListGroup.js
@@ -0,0 +1,8 @@
+export default function ListGroup() {
+  return <>
+    <ul class="list-group">
+      <li class="list-group-item">Hello world</li>
+      <li class="list-group-item">bootstrap works</li>
+    </ul>
+  </>
+}
