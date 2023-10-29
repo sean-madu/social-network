f50c27ae3ce8e36db78a57b073104e386d3f257c
sean-madu <madusean2@gmail.com>
2023-10-29 17:07:36 -0600

    Create public profile page

--- django_project/react_app/src/App.js
+++ django_project/react_app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './components/ListGroup'
@@ -6,6 +5,7 @@ import './components/ListGroup'
 import LoginForm from './logIn/LoginForm';
 import SignupForm from './logIn/SignupForm';
 import Homepage from './homepage/Homepage';
+import ProfilePage from './profilePage/Profile';
 
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -18,6 +18,7 @@ function App() {
         <Route path="/" element={<LoginForm />} />
         <Route path="/register" element={<SignupForm />} />
         <Route path="/defaultUser" element={<Homepage />} />
+        <Route path="/profile" element={<ProfilePage notUser={true} />} />
       </Routes>
     </BrowserRouter>
 
