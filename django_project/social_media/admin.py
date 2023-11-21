1c8b56c3da9c38abcea6b4f7b180f6c6af794309
TeaCookie <seaniscool22@gmail.com>
2023-11-21 14:12:28 -0700

    Donezo

--- django_project/social_media/admin.py
+++ django_project/social_media/admin.py
@@ -33,5 +33,23 @@ class PostAdminForm(forms.ModelForm):
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
 
+class LikeAdminForm(forms.ModelForm):
+    class Meta:
+        model = models.Like
+        fields = '__all__'
+
+class LikeAdmin(admin.ModelAdmin):
+    form = LikeAdminForm
+
+class CommentAdminForm(forms.ModelForm):
+    class Meta:
+        model = models.Comment
+        fields = '__all__'
+
+class CommentAdmin(admin.ModelAdmin):
+    form = CommentAdminForm
+
 admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Like, LikeAdmin)
+admin.site.register(models.Comment, CommentAdmin)
