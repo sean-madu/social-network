647db54535202a73aa81672f17f2d49622307894
sean-madu <madusean2@gmail.com>
2023-11-23 23:24:05 -0700

    refactor comment.js to use refresh tokens
    prep comments.js to usu remote connections

--- django_project/react_app/src/stream/Comment.js
+++ django_project/react_app/src/stream/Comment.js
@@ -7,34 +7,69 @@
  */
 import { useState, useEffect } from "react";
 import SERVER_ADDR from "../serverAddress";
-
-
+import { refreshCookies } from "../getCookies";
+import getCookie from "../getCookies";
 export default function Comment(props) {
 
-
+  const remote = !props.comment.id.startsWith(SERVER_ADDR)
   const [author, setAuthor] = useState("")
   const [comment, setComment] = useState("")
+  let headers;
+  if (!remote) {
+    headers = { 'Authorization': getCookie("access") }
+  }
+
 
   useEffect(() => {
-    fetch(`${SERVER_ADDR}authors/${props.comment.author}/`)
-    .then((res) => {
-      if (res.ok) {
-        res.json().then(
-          (json) => {
-            setAuthor(json.displayName)
-          }
-        )
+    fetch(`${props.comment.id.split(0, "/auth")}authors/${props.comment.author}/`, { headers })
+      .then((res) => {
+        if (res.ok) {
+          res.json().then(
+            (json) => {
+              setAuthor(json.displayName)
+            }
+          )
+        }
+        else if (res.status == 401) {
+          refreshCookies(() => {
+            headers = { 'Authorization': getCookie("access") }
+            fetch(`${SERVER_ADDR}authors/${props.comment.author}/`, { headers })
+              .then((res) => {
+                if (res.ok) {
+                  res.json().then(
+                    (json) => {
+                      setAuthor(json.displayName)
+                    }
+                  )
+                }
+              })
+          })
       }
     })
 
-    fetch(`${SERVER_ADDR}authors/${props.comment.author}/posts/${props.comment.post}/comments/${props.comment.id}`)
-    .then((res) => {
-      if (res.ok) {
-        res.json().then(
-          (json) => {
-            setComment(json.comment)
-          }
-        )
+    fetch(`${props.comment.id}`, { headers })
+      .then((res) => {
+        if (res.ok) {
+          res.json().then(
+            (json) => {
+              setComment(json.comment)
+            }
+          )
+        }
+        else if (res.status == 401) {
+          refreshCookies(() => {
+            headers = { 'Authorization': getCookie("access") }
+            fetch(`${props.comment.id}`)
+              .then((res) => {
+                if (res.ok) {
+                  res.json().then(
+                    (json) => {
+                      setComment(json.comment)
+                    }
+                  )
+                }
+              })
+          })
       }
     })
   }, []);
