acaaec62dc7a0edcfd3fd2fd45c7eaacfcbd433d
mh575 <mah5@ualberta.ca>
2023-12-08 19:11:24 -0700

    updated base url

--- django_project/social_media/views.py
+++ django_project/social_media/views.py
@@ -14,8 +14,8 @@ import bleach
 import urllib.parse
 
 
-# BASE_URL = 'https://cmput404-social-network-401e4cab2cc0.herokuapp.com/'
-BASE_URL = 'http://127.0.0.1:8000/'
+BASE_URL = 'https://cmput404-social-network-401e4cab2cc0.herokuapp.com/'
+# BASE_URL = 'http://127.0.0.1:8000/'
 
 # For authentication 
 from django.contrib.auth import authenticate, login
